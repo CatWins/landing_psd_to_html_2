3b61a6f70bbf2047087ba24a6306ddeebedd9bbe
CatWins <MIVQWE@gmail.com>
2017-03-02 05:11:50 +0500

    Added support for various screen sizes.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,6 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.menu-header_toggle').click(function(e) {
+    $('.menu-header_list').toggleClass('menu-header__active');
+    e.preventDefault();
+  });
+});
